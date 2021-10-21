20a23c1c44eb5edbef2693e166ec8198b6b28f48
Camila Conte <caxconte@gmail.com>
2021-10-21 19:09:17 -0300

    feat/get users

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 
 const app = express();
+const morgan = require('morgan');
 const cors = require('cors');
 
 const usersRouter = require('./server/routes/usersRouter');
+const ordersRouter = require('./server/routes/ordersRouter');
 
+app.use(morgan('dev'));
 app.use('/users', usersRouter);
+app.use('/orders', ordersRouter);
 
 app.use(cors());
 
