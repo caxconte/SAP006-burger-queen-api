0e6592538e0d7bed955accbf5f20a031dada3163
Camila Conte <caxconte@gmail.com>
2021-10-25 19:33:10 -0300

    feat: usersRouter done

--- server/routes/usersRouter.js
+++ server/routes/usersRouter.js
@@ -5,42 +5,115 @@ const models = require('../db/models');
 
 // RETORNA TODOS OS USERS
 router.get('/', async (req, res, next) => {
-  const users = await models.users.findAll();
-  return res.send(users);
+  models.users
+    .findAll({
+      attributes: {
+        exclude: 'password',
+      },
+    })
+    .then((response) => {
+      // console.log(response);
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      res.status(400).json({
+        code: error.code,
+        error: error.message,
+      });
+    });
 });
 
 // INSERE UM USER
 router.post('/', async (req, res, next) => {
-  // console.log(res.body);
-  const users = await models.users.create(req.body);
-  res.status(200).send(users);
+  const {
+    name, email, password, role, restaurant,
+  } = req.body;
+
+  models.users
+    .create({
+      name,
+      email,
+      password,
+      role,
+      restaurant,
+    })
+    .then((response) => {
+      res.status(201).send(response);
+    })
+    .catch((error) => {
+      res.status(400).json({
+        code: error.code,
+        error: error.message,
+      });
+    });
 });
 
 // RETORNA OS DADOS DE UM USER
 router.get('/:uid', (req, res, next) => {
-  const id = req.params.uid;
-  res.status(200).send({
-    mensagem: 'Dados de um usuário',
-    user_id: id,
-  });
+  models.users.findByPk(req.params.uid, {
+    attributes: {
+      exclude: 'password',
+    },
+  })
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      res.status(400).json({
+        code: error.code,
+        error: error.message,
+      });
+    });
 });
 
 // ALTERA OS DADOS DE UM USER
 router.put('/:uid', (req, res, next) => {
-  const id = req.params.uid;
-  res.status(200).send({
-    mensagem: 'Alterando dados de um usuário',
-    user_id: id,
-  });
+  const {
+    name, email, password, role, restaurant,
+  } = req.body;
+
+  models.users
+    .update({
+      name,
+      email,
+      password,
+      role,
+      restaurant,
+    },
+    {
+      where: {
+        id: req.params.uid,
+      },
+    })
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      res.status(400).json({
+        code: error.code,
+        error: error.message,
+      });
+    });
 });
 
 // DELETA UM USUARIO
 router.delete('/:uid', (req, res, next) => {
-  const id = req.params.uid;
-  res.status(200).send({
-    mensagem: 'Usuário deletado',
-    user_id: id,
-  });
+  models.users.destroy({
+    where: {
+      id: req.params.uid,
+    },
+  })
+    .then(() => {
+      res.status(200).send({
+        message: 'User successfully deleted!',
+      });
+    })
+    .catch((error) => {
+      res.status(400).json({
+        code: error.code,
+        error: error.message,
+      });
+    });
 });
 
 module.exports = router;
